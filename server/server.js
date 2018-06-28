24851c26997d61ebb9f8c900b3b950ae336d8004
Courtney <cpswedenburg@gmail.com>
2018-06-28 16:42:34 -0600

    added the first part of user-auth

--- server/server.js
+++ server/server.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 app.use(morgan('dev'));
+app.use('/recipe', require('./routes/recipe'));
+// app.use('/auth', require('./routes/auth'));
 
-mongoose.connect('mongodb://localhost/recipe-database',
-{useMongoClient: true},
- (err) => {
-    if(err) throw err
+mongoose.connect('mongodb://localhost/recipe-database', (err) => {
+    if (err) console.log(err);
     console.log('connected to the database');
 })
 
-app.use('/recipe', require('./routes/recipe'))
 
 app.listen(5050, () => {
     console.log('server is running on port 5050')
