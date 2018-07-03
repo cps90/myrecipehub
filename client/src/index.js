9eab30bd97e42571faafaef429201fd086f77f04
spencerbackman <backmanspencer99@gmail.com>
2018-07-03 10:39:15 -0600

    fixed issues with logging in and staying logged in

--- client/src/index.js
+++ client/src/index.js
@@ -5,11 +5,15 @@ import { Provider } from 'react-redux';
 import store from './redux';
 import { BrowserRouter} from 'react-router-dom';
 
+store.subscribe(() => {
+    console.log(store.getState())
+})
+
 ReactDOM.render(
 <Provider store={store}>
-<BrowserRouter>
-    <App />
-</BrowserRouter>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 </Provider>,
-    document.getElementById('root')
+document.getElementById('root')
 )
