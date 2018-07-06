8d6350e2e465c35fd961e1ff88548cb03fc623c6
cps90 <cpswedenburg@gmail.com>
2018-07-06 14:50:27 -0600

    last-minute-styling

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -5,14 +5,17 @@ import { Link } from 'react-router-dom';
 function Home(props){
         return (
             <div>
-                <Link className="homelinks" to="./apps">Appetizers </Link>
-                <Link className="homelinks" to="./breakfast">Breakfast </Link>
-                <Link className="homelinks" to="./cocktails">Cocktails </Link>
-                <Link className="homelinks" to="./desserts">Desserts </Link>
-                <Link className="homelinks" to="./maincourse">Main Course </Link>
-                <Link className="homelinks" to="./sidedishes"> Side Dishes </Link>
-                <Link className="homelinks" to="./new">New Recipe </Link>
-            </div>
+                <div className="homepage">
+                        <Link className="homelinks appetizers" to="./apps"><span>Appetizers</span></Link>
+                        <Link className="homelinks breakfast" to="./breakfast"><span>Breakfast</span></Link>
+                        <Link className="homelinks cocktails" to="./cocktails"><span>Cocktails</span></Link>
+                        <Link className="homelinks desserts" to="./desserts"><span>Desserts</span></Link>
+                        <Link className="homelinks maincourse" to="./maincourse"><span>Main Course</span></Link>
+                        <Link className="homelinks sides" to="./sidedishes"><span>Side Dishes</span></Link> 
+                </div> 
+                <Link className="new-recipe" to="./new">New Recipe </Link>  
+            </div>    
+          
         )
 }
 
